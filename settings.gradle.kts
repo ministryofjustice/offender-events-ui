6ff55566e752509e63fb1206313b4443bb826083
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2020-05-01 13:06:05 +0100

    DT-778 Get a Spring Boot project up and running (#1)

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "offender-events-ui"
---
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/App.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.hmpps.offenderevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
