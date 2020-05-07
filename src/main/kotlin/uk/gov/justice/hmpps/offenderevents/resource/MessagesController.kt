8cf43a11753f5f1212e591d28609de32884a34ec
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-07 15:31:34 +0100

    DT-778 Add a filter

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/resource/MessagesController.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/resource/MessagesController.kt
@@ -5,6 +5,7 @@ import com.microsoft.applicationinsights.core.dependencies.google.gson.Gson
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
 import uk.gov.justice.hmpps.offenderevents.service.OffenderEventStore
 import uk.gov.justice.hmpps.offenderevents.service.StoredMessage
@@ -16,7 +17,7 @@ class MessagesController {
 
   companion object {
     inline fun <reified T> fromJson(json: String): T {
-      return Gson().fromJson(json, object: TypeToken<T>(){}.type)
+      return Gson().fromJson(json, object : TypeToken<T>() {}.type)
     }
   }
 
@@ -24,8 +25,17 @@ class MessagesController {
   private lateinit var offenderEventStore: OffenderEventStore
 
   @GetMapping("/messages")
-  fun messages(): ModelAndView {
-    return ModelAndView("index", mutableMapOf("displayMessages" to offenderEventStore.getAllMessages().map(::transformMessage).toList()))
+  fun messages(
+      @RequestParam(name = "event-type-filter", required = false) eventTypeFilter: String?,
+      @RequestParam(name = "text-filter", required = false) textFilter: String?): ModelAndView {
+    return ModelAndView("index",
+        mutableMapOf(
+            "displayMessages" to offenderEventStore.getAllMessages(eventTypeFilter, textFilter).map(::transformMessage).toList(),
+            "allEventTypes" to offenderEventStore.getAllEventTypes(),
+            "eventTypeFilter" to eventTypeFilter,
+            "textFilter" to textFilter
+        )
+    )
   }
 
   fun transformMessage(storedMessage: StoredMessage): DisplayMessage {
