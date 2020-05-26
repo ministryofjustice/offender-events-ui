70861816f3ba14d34ba6528fea7f218e6a33ae80
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-05-26 13:25:34 +0100

    DT-868 Upgrade to handle CVEs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.3.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.4.0"
   kotlin("plugin.spring") version "1.3.72"
   kotlin("plugin.jpa") version "1.3.72"
 }
