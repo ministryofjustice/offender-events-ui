6e5a00cbc69f093554f30b40fd4aaac25d5d2b5c
Peter Phillips <peter@greenthistle.co.uk>
2020-09-03 11:34:22 +0100

    DT-1080: 🔒 Suppress kotlin milestone vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.2"
   kotlin("plugin.spring") version "1.4.0"
   kotlin("plugin.jpa") version "1.4.0"
 }
