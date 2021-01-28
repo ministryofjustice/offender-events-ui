ef8788c4eb5ee44c33f5c90680d4aadbcca8a2ab
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-01-28 15:33:22 +0000

    DT-1547 Investigate wtf

--- src/test/kotlin/uk/gov/justice/hmpps/offenderevents/AppTest.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offenderevents/AppTest.kt
@@ -4,11 +4,15 @@ import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.annotation.DirtiesContext.ClassMode
+import org.springframework.test.annotation.DirtiesContext.MethodMode.AFTER_METHOD
 import org.springframework.test.context.ActiveProfiles
 import java.net.URL
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 class AppTest {
 
   @LocalServerPort
