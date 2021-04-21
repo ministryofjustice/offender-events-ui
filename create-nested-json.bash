cbd61be516325423971d4db6561be8922b07986c
petergphillips <peter@greenthistle.co.uk>
2021-04-21 16:01:34 +0100

    DT-1949: ⬆️ Update to latest localstack (#46)

--- create-nested-json.bash
+++ create-nested-json.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws --endpoint-url=http://localhost:4575 sns publish \
+aws --endpoint-url=http://localhost:4566 sns publish \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events \
     --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"INVALID_JSON"}}' \
     --message '{"offenderIdDisplay":"G0373GG","offenders":[{"offenderId":1025558,"bookings":[{"offenderBookId":12678}]}]}'
--- localstack/setup-sns.sh
+++ localstack/setup-sns.sh
@@ -2,12 +2,12 @@
 export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
-aws --endpoint-url=http://localhost:4575 sns create-topic --name offender_events
+aws --endpoint-url=http://localhost:4566 sns create-topic --name offender_events
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name offender_events_ui_dlq
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name offender_events_ui_queue
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/offender_events_ui_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:offender_events_ui_dlq\"}"}'
-aws --endpoint-url=http://localhost:4575 sns subscribe \
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name offender_events_ui_dlq
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name offender_events_ui_queue
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/offender_events_ui_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:offender_events_ui_dlq\"}"}'
+aws --endpoint-url=http://localhost:4566 sns subscribe \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events \
     --protocol sqs \
-    --notification-endpoint http://localhost:4576/queue/offender_events_ui_queue
+    --notification-endpoint http://localhost:4566/queue/offender_events_ui_queue
