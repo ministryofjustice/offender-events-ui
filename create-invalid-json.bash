910373a04b2a584e3c9fb29fd1e6ee09446881bd
Peter Phillips <peter@greenthistle.co.uk>
2021-04-21 13:36:37 +0100

    DT-1949: ⬆️ Update to latest localstack

--- create-invalid-json.bash
+++ create-invalid-json.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws --endpoint-url=http://localhost:4575 sns publish \
+aws --endpoint-url=http://localhost:4566 sns publish \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events \
     --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"INVALID_JSON"}}' \
     --message '{"eventType":"INVALID_JSON","eventDatetime":"2020-01-13T11:33:23.790725","whoops this is invalid"}'
