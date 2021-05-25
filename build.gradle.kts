ee0dc00195434135d5ec3db931c5d7be45c0c052
Peter Phillips <peter@greenthistle.co.uk>
2021-05-25 12:23:18 +0100

    DT-2070: ⬆️  Upgrade to spring boot 2.5.0 and kotlin 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.2.1"
-  kotlin("plugin.spring") version "1.5.0"
-  kotlin("plugin.jpa") version "1.5.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0-beta"
+  kotlin("plugin.spring") version "1.5.10"
+  kotlin("plugin.jpa") version "1.5.10"
 }
 
 dependencies {
@@ -10,14 +10,14 @@ dependencies {
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   implementation("org.springframework:spring-jms")
 
-  implementation("com.google.code.gson:gson:2.8.6")
+  implementation("com.google.code.gson:gson:2.8.7")
 
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.3")
 
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.26.0")
   testImplementation("org.testcontainers:localstack:1.15.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
 }
