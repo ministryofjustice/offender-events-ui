c492dd90a1fe24291953115bbeaa0cccd7cf534f
petergphillips <peter@greenthistle.co.uk>
2021-05-26 14:14:49 +0100

    DT-2070: 🐛 Bump to v3 (#55)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.0"
   kotlin("plugin.spring") version "1.5.10"
   kotlin("plugin.jpa") version "1.5.10"
 }
