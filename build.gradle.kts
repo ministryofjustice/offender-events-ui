0c9d8cc076a22797cb06c58942007b8ba6dff4e7
petergphillips <peter@greenthistle.co.uk>
2021-09-08 10:08:54 +0100

    DT-2510: 🔧 Add upgrade to Dockerfiles and upgrade libraries (#76)
    * DT-2510: 🔧 Add upgrade to Dockerfiles and upgrade libraries
    
    * DT-2510: 🔧 Also fix java / kotlin jdk versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.7-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.7"
   kotlin("plugin.spring") version "1.5.30"
   kotlin("plugin.jpa") version "1.5.30"
 }
@@ -12,21 +12,25 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:0.9.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
 
-  implementation("com.google.code.gson:gson:2.8.7")
+  implementation("com.google.code.gson:gson:2.8.8")
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.3")
+  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
 
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.28.0")
   testImplementation("org.testcontainers:localstack:1.16.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
 }
 
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(16))
+}
+
 tasks {
-  compileKotlin {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
       jvmTarget = "16"
     }
