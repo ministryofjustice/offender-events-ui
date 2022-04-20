8c6066aae145024fa02f2e712f39ba6756c3582e
Peter Phillips <peter@greenthistle.co.uk>
2022-04-20 09:42:21 +0100

    SDI-171: 🔒️ Fix security issues by bumping versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4-beta"
-  kotlin("plugin.spring") version "1.6.20"
-  kotlin("plugin.jpa") version "1.6.20"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
 }
 
 configurations {
@@ -21,8 +21,8 @@ dependencies {
   // pinned at 4.0.1 due to security vulnerability with 4.0.0 - remove when thymeleaf upgraded
   implementation("org.apache.groovy:groovy:4.0.1")
 
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.33.0")
-  testImplementation("org.testcontainers:localstack:1.17.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.34.0")
+  testImplementation("org.testcontainers:localstack:1.17.1")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
 }
