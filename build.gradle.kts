b30bd136a61c6e686e4e5734d7aad8ca87738435
petergphillips <peter@greenthistle.co.uk>
2022-10-05 09:39:47 +0100

    SDI-409: ⬆️  Grab latest spring boot gradle plugin (#148)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.2-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.5.4"
   kotlin("plugin.spring") version "1.7.10"
   kotlin("plugin.jpa") version "1.7.10"
 }
@@ -12,7 +12,7 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.10")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.11")
 
   implementation("com.google.code.gson:gson:2.9.1")
 
@@ -22,7 +22,7 @@ dependencies {
   implementation("org.apache.groovy:groovy:4.0.5")
 
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
-  testImplementation("org.testcontainers:localstack:1.17.3")
+  testImplementation("org.testcontainers:localstack:1.17.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
   testImplementation("commons-io:commons-io:2.11.0") // override vulnerable version
