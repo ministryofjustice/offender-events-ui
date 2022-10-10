e67e3c8e9e52f71ba442ff3d47b9f745cfad7392
Peter Phillips <peter@greenthistle.co.uk>
2022-10-10 11:20:37 +0100

    SDI-414: 🔒️ Enable veracode scans

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.11")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.12")
 
   implementation("com.google.code.gson:gson:2.9.1")
 
@@ -21,7 +21,7 @@ dependencies {
   // pinned at 4.0.1 due to security vulnerability with 4.0.0 - remove when thymeleaf upgraded
   implementation("org.apache.groovy:groovy:4.0.5")
 
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.35.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.0")
   testImplementation("org.testcontainers:localstack:1.17.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
