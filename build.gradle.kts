0c2db328095e3a9023df10699b9eef31d219e20e
Peter Phillips <peter@greenthistle.co.uk>
2023-03-01 10:41:57 +0000

    SDIT-513: ⬆️  Bump to fixed gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.1-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.1-beta-2"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
 }
