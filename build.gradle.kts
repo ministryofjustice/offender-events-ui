2f3b197bbaa93a1a789c9aab207f837b16710b9f
Peter Phillips <peter@greenthistle.co.uk>
2023-03-23 15:28:37 +0000

    SDIT-703: ⬆️  Upgrade to latest to fix security issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.1-beta-3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.3-beta"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
 }
@@ -22,8 +22,8 @@ dependencies {
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.0")
-  implementation("org.apache.groovy:groovy:4.0.9")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.23.0")
+  implementation("org.apache.groovy:groovy:4.0.10")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.24.0")
 
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.1")
   testImplementation("org.testcontainers:localstack:1.17.6")
