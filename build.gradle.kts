60edb4d1f202b6186845fa4a0231aec91e232b2c
petergphillips <peter@greenthistle.co.uk>
2023-03-30 12:03:18 +0100

    SDIT-703: ⬆️  Upgrade to latest to suppress false positive (#189)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.3-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.3-beta-2"
   kotlin("plugin.spring") version "1.8.10"
   kotlin("plugin.jpa") version "1.8.10"
 }
@@ -21,11 +21,11 @@ dependencies {
   implementation("com.google.code.gson:gson:2.10.1")
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.0")
+  implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.2.1")
   implementation("org.apache.groovy:groovy:4.0.10")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.24.0")
 
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.36.1")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.37.0")
   testImplementation("org.testcontainers:localstack:1.17.6")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
