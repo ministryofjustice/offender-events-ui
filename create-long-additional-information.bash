fbd4ca7ae37ad5a89b0b81a673c1d1a0a6261a84
petergphillips <peter@greenthistle.co.uk>
2023-10-25 16:30:21 +0100

    SDIT-746: 💄 Ensure long lines get wrapped (#228)

---
+++ create-long-additional-information.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+aws --endpoint-url=http://localhost:4566 sns publish \
+    --topic-arn arn:aws:sns:eu-west-2:000000000000:offender_events \
+    --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"prison-offender-events.prisoner.received"}}' \
+    --message '{"version":"1.0","description":"A prisoner has been received into prison","occurredAt":"2023-10-25T15:21:47+01:00","personReference":"{identifiers=[{type=NOMS, value=A6887DZ}]}","additionalInformation":"{nomsNumber=A6887DZ, reason=ADMISSION, probableCause=REMAND, source=PRISON, details=ACTIVE IN:ADM-N, currentLocation=IN_PRISON, prisonId=LII, nomisMovementReasonCode=N, currentPrisonStatus=UNDER_PRISON_CARE}"}'
