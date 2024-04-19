7bbf06095358b440d0509cb4bd6627db68c1723c
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-04-19 11:09:06 +0100

    SDIT-1677: ⬆️ Upgrade to latest spring boot (#251)
    * SDIT-1677: ⬆️ Upgrade to latest spring boot
    
    * SDIT-1677: 🔒️ Remove custom suppressions as spring boot now upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.5"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.6"
   kotlin("plugin.spring") version "1.9.23"
   kotlin("plugin.jpa") version "1.9.23"
 }
@@ -12,32 +12,26 @@ repositories {
   mavenCentral()
 }
 
-dependencyCheck {
-  // Thymeleaf through 3.1.1.RELEASE, as used in spring-boot-admin (aka Spring Boot Admin) through 3.1.1 and other products, allows sandbox bypass via crafted HTML. This may be relevant for SSTI (Server Side Template Injection) and code execution in spring-boot-admin if MailNotifier is enabled and there is write access to environment variables via the UI.
-  // Not relevant for us as can't pass html in.  Suppressed.
-  suppressionFiles.add("thymeleaf-suppressions.xml")
-}
-
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.2")
 
   implementation("com.google.code.gson:gson:2.10.1")
   implementation("com.google.guava:guava:33.1.0-jre")
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.3.0")
-  implementation("org.apache.groovy:groovy:4.0.20")
+  implementation("org.apache.groovy:groovy:4.0.21")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.33.1")
 
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.2.7")
   testImplementation("org.testcontainers:localstack:1.19.7")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.688")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.704")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
-  testImplementation("commons-io:commons-io:2.15.1") // override vulnerable version
+  testImplementation("commons-io:commons-io:2.16.1") // override vulnerable version
 }
 
 kotlin {
