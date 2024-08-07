8fde1578c165dd4f436c1ae5f23389c4a6d3d077
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-08-07 10:38:16 +0100

    SDIT-1919: ⬆️ Upgrade to latest hmpps spring boot (#264)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.2-beta"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.2"
   kotlin("plugin.spring") version "2.0.0"
   kotlin("plugin.jpa") version "2.0.0"
 }
@@ -15,9 +15,9 @@ configurations {
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.2-beta-3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.3")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.2.0")
 
   implementation("com.google.code.gson:gson:2.11.0")
   implementation("com.google.guava:guava:33.2.0-jre")
@@ -25,12 +25,12 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.3.0")
   implementation("org.apache.groovy:groovy:4.0.22")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.6.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.4.0")
 
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.4.1")
-  testImplementation("org.testcontainers:localstack:1.20.0")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.765")
-  testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
+  testImplementation("org.testcontainers:localstack:1.20.1")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.767")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
 }
 
