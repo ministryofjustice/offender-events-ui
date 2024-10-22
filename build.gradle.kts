fc2c462bf560a4e8fb152e1f1dfb838d20bb87ac
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-22 13:51:42 +0100

    SDIT-2141: ⬆️ Upgrade to hmpps spring boot 6.0.7 for path directory traversal suppression (#274)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.7"
   kotlin("plugin.spring") version "2.0.20"
   kotlin("plugin.jpa") version "2.0.20"
 }
@@ -17,19 +17,19 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.7")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.4.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.1")
 
   implementation("com.google.code.gson:gson:2.11.0")
-  implementation("com.google.guava:guava:33.3.0-jre")
+  implementation("com.google.guava:guava:33.3.1-jre")
 
   implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.3.0")
   implementation("org.apache.groovy:groovy:4.0.23")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.6.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
 
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:3.4.1")
-  testImplementation("org.testcontainers:localstack:1.20.1")
-  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.772")
+  testImplementation("org.testcontainers:localstack:1.20.2")
+  testImplementation("com.amazonaws:aws-java-sdk-core:1.12.777")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
   testImplementation("it.ozimov:embedded-redis:0.7.3") { exclude("org.slf4j", "slf4j-simple") }
 }
