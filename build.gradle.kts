9c429e932c69e09da9a1a42de6b70725576bffcd
Mike Halma <58170926+mikehalmamoj@users.noreply.github.com>
2025-02-20 13:09:46 +0000

    SDIT-2593 Upgrade to full verion of kotlin lib (#291)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ configurations {
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.0")
   implementation("org.springframework.boot:spring-boot-starter-data-redis")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.3.0")
 
